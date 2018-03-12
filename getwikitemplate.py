f237015ab0ff19bc4b35dc963a621af1c984ec32
Antonin Fouques <afouques@genymobile.com>
2018-03-12 16:17:28 +0100

    Setup for Genymobile wiki template creator

---
+++ getwikitemplate.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python2
+
+import sys
+from jiraauth import jclient as jira
+
+'''
+Synopsis:  This script runs a JQL search to get the "Done" issues in the
+last x weeks in the project y and format it in a wiki template
+Example: getwikitemplate.py [projectName] [iteration number of week OR since date in format YYYY-MM-DD_HH:mm]
+'''
+
+project = None
+time = 0
+date = None
+
+if len(sys.argv) > 1:
+    project = sys.argv[1]
+else:
+    project = raw_input("Project Name: ")
+
+if len(sys.argv) > 2:
+    time = sys.argv[2]
+else:
+    time = raw_input("iteration length in weeks OR since date in format YYYY-MM-DD_HH:mm : ")
+if len(time) <= 3:
+    date = "-" + time + "w"
+else:
+    date = "'" + time.replace("_", " ") + "'"
+
+
+
+issues = jira.search_issues("project = " + project + " AND issuetype in (Story, Bug) AND status = Done AND resolved > " + date + " ORDER BY resolved ASC")
+
+print "\nList of issues\n------------------------------"
+
+teamsStory = {}
+teamsBug = {}
+
+for x in issues:
+    if x.fields.customfield_11400 == None:
+        sys.exit("Issue " + x.key + " has no Team! Fill it and run the script again --> https://genymobile.atlassian.net/browse/" + x.key)
+    print x.key + " | " + x.fields.summary + " | " + x.fields.issuetype.name + " | " + x.fields.customfield_11400[0]
+    if x.fields.issuetype.name == "Story":
+        if not teamsStory.has_key(x.fields.customfield_11400[0]):
+            teamsStory[x.fields.customfield_11400[0]] = [x]
+        else:
+            teamsStory[x.fields.customfield_11400[0]].append(x)
+    else:
+        if not teamsBug.has_key(x.fields.customfield_11400[0]):
+            teamsBug[x.fields.customfield_11400[0]] = [x]
+        else:
+            teamsBug[x.fields.customfield_11400[0]].append(x)
+        
+
+print "------------------------------"
+print "         Wiki Template"
+print "------------------------------"
+print ""
+print "[[Accueil]] > [[Genymotion]] > [[Genymotion Product Management and Scrum]] > [[Genymotion Demos and Retros]] >> [[GenymotionNAME OF THE ITERATIONDemo]]"
+print ""
+print "==Objectives ''(by priority)''=="
+print ""
+print "* TODO: FILL THIS"
+print ""
+print "==Stats=="
+print ""
+print "Start date: TODO: FILL THIS<br />"
+print "End date: TODO: FILL THIS<br />"
+print "<br />"
+print "13 Dev and QA<br />"
+print "2 designers<br />"
+print "1 doc<br />"
+print "4 PO<br />"
+print "0 SM<br />"
+print "TODO ADJUST NUMBERS<br />"
+print "TODO: FILL THIS days/(wo)men in 4 teams"
+print ""
+print "==Main facts=="
+print ""
+print "* TODO: FILL THIS"
+print ""
+print "==Features=="
+print ""
+for team_key in teamsStory.keys():
+    print "------------------------------------------"
+    print ""
+    print "===[Motion " + team_key + "]==="
+    print "'''FO: TODO: FILL THIS'''<br/>"
+    print "'''TODO: FILL TEAM MEMBERS'''"
+    print ""
+    print "TODO: FILL TEAM GOAL"
+    print ""
+    print "'''What was Done?'''"
+    if teamsBug.get(team_key) != None:
+        print "* Bug Fixed : " + str(len(teamsBug.get(team_key)))
+    if teamsStory.get(team_key) != None:
+        print "{| class='wikitable'"
+        print "|-"
+        print "! Jira Key !! Summary !! Demo?"
+        print "|-"
+        for x in teamsStory.get(team_key):
+            description = x.fields.description.split("\r\n")
+            print "| [https://genymobile.atlassian.net/browse/" + x.key + " " + x.key +"] || '''" + x.fields.summary + "'''<br />"
+            if len(description) > 0: print "" + description[0] + "<br />"
+            if len(description) > 1: print "" + description[1] + "<br />"
+            if len(description) > 2: print "" + description[2]
+            print "| -" # TODO FILL WHO :<br />
+            # TODO FILL WHERE print [https://drive.google.com/open?id=0B0InYU6y8GpUQXBPSUNMbUNKWk0 Video]
+            print "|-"
+        print "|}"
--- jiraauth.py
+++ jiraauth.py
@@ -1,4 +1,3 @@
-
 # NOTE: you must have jira-python in your PYTHONPATH for this to work
 from jira.client import JIRA
 from jira.jirashell import process_config
@@ -8,24 +7,22 @@ This script assumes the existence of ~/.jira-python/jirashell.ini
 with format like:
 
 [options]
-server = https://eucalyptus.atlassian.net
+server = https://genymobile.atlassian.net
 
 [basic_auth]
-username = agrimm
-password = s3kr1t
+username = afouques
+password = (optional)YourPassword
 
 If you do not wish to use a config file, you can initialize a client
 like this:
 
-jclient = JIRA(options, basic_auth=('agrimm', 's3kr1t'))
+jclient = JIRA(options, basic_auth=('afouques', 'YourPassword'))
 
 # This can also be made to work with oauth credentials
 # See https://jira-python.readthedocs.org/en/latest/#authentication
 '''
 
 options, basic_auth, oauth = process_config()
-if not options.has_key('server'):
-    options['server'] = 'https://eucalyptus.atlassian.net'
 
 jclient = None
 password = None
@@ -37,7 +34,7 @@ else:
         password = basic_auth['password']
     else:
         import getpass
-        password = getpass.getpass('Eucalyptus Jira password: ') 
+        password = getpass.getpass(options['server'] + ' password: ') 
     jclient = JIRA(options=options,
                    basic_auth=(basic_auth['username'], 
                                password))
